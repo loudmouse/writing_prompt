4e8dd5fa1198876a2972b80e9868f5fd584c1309
Nolan Neeley <nolanneeley@gmail.com>
2018-09-10 22:51:53 -0500

    setup integration test for creating a prompt

--- test/integration/prompt_flow_test.rb
+++ test/integration/prompt_flow_test.rb
@@ -3,6 +3,18 @@ require 'test_helper'
 class PromptFlowTest < ActionDispatch::IntegrationTest
   test "can see the prompt#index as home page" do
     get "/"
-    assert_select "h1", "Prompt/Index"
+    assert_select "h1", "Daily Writing Prompts"
+  end
+
+  test "can create a prompt" do
+    get "/prompts/new"
+    assert_response :success
+
+    post "/prompts",
+      params: { prompt: { random_word: "road trip", body: "On the road again..."}}
+    assert_response :redirect
+    follow_redirect!
+    assert_response :sucess
+    assert_select "h2", "road trip"
   end
 end
