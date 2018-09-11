00b120cd4c539142641e86cc512a91f190ac55d6
Nolan Neeley <nolanneeley@gmail.com>
2018-09-10 19:19:29 -0500

    added test on prompst controller for the index action

--- test/controllers/prompts_controller_test.rb
+++ test/controllers/prompts_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PromptsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get prompts_url
+    assert_response :success
+  end
 end
