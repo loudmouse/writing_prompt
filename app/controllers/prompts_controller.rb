bd6b76f13c13b659e2aa54c75b5ece55d3ec3155
Nolan Neeley <nolanneeley@gmail.com>
2018-09-19 16:37:38 -0500

    get word and word count to show up on show page

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -7,6 +7,22 @@ class PromptsController < ApplicationController
 
   def new
     @prompt = Prompt.new
+
+    client = OxfordDictionary::Client.new(app_id: Figaro.env.app_id, app_key: Figaro.env.app_key)
+    client = OxfordDictionary.new(app_id: Figaro.env.app_id, app_key: Figaro.env.app_key)
+
+
+    # word = client.entry("car")
+
+    # creates a list of 10 nouns
+    word_list = client.wordlist(lexicalCategory: 'Noun', limit: '20', regions: 'US')
+    # generates random number from the list, minus 1 to 0-index it
+    word_index = rand(5)-1
+    @random_word = word_list.results[word_index].word
+
+    # @definition = @random_word.lexical_entries[0].entries[0].senses[0].definitions[0]
+
+    # @example = word.lexical_entries[0].entries[0].senses[0].examples[0].text
   end
 
   def create
@@ -22,20 +38,20 @@ class PromptsController < ApplicationController
 
   def show
     @prompt = Prompt.find(params[:id])
+    @random_word = @prompt.random_word
+    @body = @prompt.body
+    @word_count = @body.split.size
 
 
 
-    client = OxfordDictionary::Client.new(app_id: Figaro.env.app_id, app_key: Figaro.env.app_key)
-    client = OxfordDictionary.new(app_id: Figaro.env.app_id, app_key: Figaro.env.app_key)
-
-
-    word = client.entry("banana")
-    @random_word = word
-    @definition = word.lexical_entries[0].entries[0].senses[0].definitions[0]
-    @example = word.lexical_entries[0].entries[0].senses[0].examples[0].text
-
-
-
+    # client = OxfordDictionary::Client.new(app_id: Figaro.env.app_id, app_key: Figaro.env.app_key)
+    # client = OxfordDictionary.new(app_id: Figaro.env.app_id, app_key: Figaro.env.app_key)
+    #
+    #
+    # word = client.entry("banana")
+    # @random_word = word
+    # @definition = word.lexical_entries[0].entries[0].senses[0].definitions[0]
+    # @example = word.lexical_entries[0].entries[0].senses[0].examples[0].text
   end
 
   private
