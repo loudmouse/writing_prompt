59d89e6a6e4b93f89a1571cfe453ccfbdaa7aec4
Nolan Neeley <nolanneeley@gmail.com>
2018-12-07 22:36:54 -0600

    updated wordnik ENV

--- config/initializers/wordnik.rb
+++ config/initializers/wordnik.rb
@@ -1,5 +1,5 @@
 Wordnik.configure do |config|
-  config.api_key = ENV["wordnik_api_key"]             # required
+  config.api_key = ENV["WORDNIK_API_KEY"]             # required
   # config.username = 'bozo'                    # optional, but needed for user-related functions
   # config.password = 'cl0wnt0wn'               # optional, but needed for user-related functions
   config.response_format = 'json'             # defaults to json, but xml is also supported
