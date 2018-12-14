9f57d30aea644da2297a14a51a5ce12e66ae0701
Nolan Neeley <nolanneeley@gmail.com>
2018-12-14 14:33:29 -0600

    tidied up the timer js

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -2,29 +2,23 @@ document.addEventListener("turbolinks:load", function() {
 
   let countdown;
   const timerDisplay = document.querySelector('.display__time-left');
-  const endTime = document.querySelector('.display__end-time');
-  const buttons = document.querySelectorAll('[data-time]');
+  const button = document.getElementById('timer_start_button');
 
   function timer(seconds) {
-  // clear any existing timers
-  clearInterval(countdown);
+    clearInterval(countdown);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
 
-  const now = Date.now();
-  const then = now + seconds * 1000;
-  displayTimeLeft(seconds);
-  displayEndTime(then);
-
-  countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
-    if(secondsLeft < 0) {
-      clearInterval(countdown);
-      alert("Time's Up!");
-      return;
-    }
-    // display it
-    displayTimeLeft(secondsLeft);
-  }, 1000);
+    countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      if(secondsLeft < 0) {
+        clearInterval(countdown);
+        alert("Time's Up!");
+        return;
+      }
+      displayTimeLeft(secondsLeft);
+    }, 1000);
   }
 
   function displayTimeLeft(seconds){
@@ -35,25 +29,11 @@ document.addEventListener("turbolinks:load", function() {
   timerDisplay.textContent = display;
   }
 
-  function displayEndTime(timestamp){
-  const end = new Date(timestamp);
-  const hour = end.getHours();
-  const minutes = end.getMinutes();
-  endTime.textContent = `Write Until: ${hour}:${minutes}`;
-  }
-
   function startTimer() {
   const seconds = parseInt(this.dataset.time);
   timer(seconds);
   }
 
-  buttons.forEach(button => button.addEventListener('click', startTimer));
-  document.customForm.addEventListener('submit', function(e) {
-  // e.preventDefault();
-  const mins = this.minutes.value;
-  console.log(mins);
-  timer(mins * 60);
-  this.reset();
-  });
+  button.addEventListener('click', startTimer);
 
 });
