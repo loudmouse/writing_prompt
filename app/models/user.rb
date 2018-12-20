de2f36a924bdd5458dd5658cbf67371320ba5c80
Nolan Neeley <nolanneeley@gmail.com>
2018-12-20 14:46:11 -0600

    added custom error message for length validation on bio in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :username, presence: true
-  validates :bio, length: { maximum: 200 }
+  validates :bio, length: { maximum: 200,
+                            too_long: "Whoa there! Save your keystrokes for your daily writing. %{count} characters is the maximum allowed here." }
 
   has_many :prompts
 
